f11ee28934cf8919f1c2b9494fa86b2b1e7141b2
Scorpion <scorpion@Virtualbox.myguest.virtualbox.org>
2023-02-17 02:20:40 +0000

    Hot fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,13 @@
 #include<stdio.h>
-#include<string.h>
+/**
+ * main - Entry point
+ * Description: 'Prints out exactly the variable value'
+ * Return: Always 0 (succes)
+ */ 
 int main(void)
 {
-	char my_var[] = "Programming is like building a multilingual puzzle";
+char my_var[] = "\"Programming is like building a multilingual puzzle";
 
-	puts(my_var);
-	return (0);
+puts(my_var);
+return (0);
 }
