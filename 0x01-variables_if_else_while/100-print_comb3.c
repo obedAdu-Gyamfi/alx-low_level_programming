bd71356d3690e5dc25e8160b2071973f74deaf0d
Scorpion <scorpion@Virtualbox.myguest.virtualbox.org>
2023-02-19 15:06:42 +0000

    Task 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ *
+ */
+int main(void)
+{
+int num;
+int n;
+for (num = '0'; num < '9' ; num++)
+{
+	for (n = num + 1; n <= '9'; n++)
+	{
+		if (n != num)
+		{
+			putchar(num);
+			putchar(n);
+			if ((num == '8') && (num == '9'))
+				continue;
+		putchar(',');
+		putchar(' ');
+		}
+	}
+}
+putchar('\n');
+return (0);
+
+}
