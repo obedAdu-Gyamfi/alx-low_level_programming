dbd90e457560c9e05e673b4a2de2b5e180e0711c
Scorpion <scorpion@Virtualbox.myguest.virtualbox.org>
2023-02-23 01:51:10 +0000

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main- Entry point.
+ *
+ * Return: Always 0 on success.
+ *
+ */
+int main(void)
+{
+	int alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		_putchar(alpha);
+	}
+	_putchar('\n');
+	return (0);
+}
