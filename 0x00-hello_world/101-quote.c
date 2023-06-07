b340244f94df5f3cb3093158bd7f89cbefa640f2
Scorpion <scorpion@Virtualbox.myguest.virtualbox.org>
2023-06-07 12:16:11 +0100

    Update with executable linus scripts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Failure)
+ */
+int main(void)
+{
+	char my_var[] = "and that piece of art is useful\" - Dora Korpar, 2o15-10-19\n"
+
+	write(1, my_var, 59);
+	return (1);
+}
