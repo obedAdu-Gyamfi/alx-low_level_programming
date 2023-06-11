1cdda39443ecdcf368aec57336f09b6de11dafd4
Scorpion <scorpion@Virtualbox.myguest.virtualbox.org>
2023-06-11 16:15:10 +0100

    Update

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *
+ * rev_string - Entry point.
+ *
+ * Description: This function reverses a string.
+ *
+ * @s: this is the a pointer to the string parameter that
+ * is passed to the function.
+ *
+ * Return: Nothing.
+ *
+ */
+
+void rev_string(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	len = len - 1;
+	while (len != - 1 )
+	{
+		_putchar(s[len--]);
+	}
+	_putchar('\n');
+}
