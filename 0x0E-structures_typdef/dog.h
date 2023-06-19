4a9f605510337de9602810a374bcb1b6f14991a0
Obed <adugyamfiobed.tpp3@gmail.com>
2023-06-19 16:05:34 +0100

    Struct/typedef

---
+++ 0x0E-structures_typdef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_ONE
+#define DOG_ONE
+
+/**
+ * struct dog - Short description
+ * @name: Name
+ * @age: age
+ * @owner: Owner
+ *
+ * Description: This is a struct on structs.
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
