61aaa06f885a8d8a9df6dd67acb42da15accbc93
Obed <adugyamfiobed.tpp3@gmail.com>
2023-06-20 00:00:37 +0100

    typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,7 +17,7 @@ typedef struct dog
 	char *name;
 	float age;
 	char *owner;
-} dog;
+} dog, dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
