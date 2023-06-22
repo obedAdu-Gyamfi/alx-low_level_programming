6a049688f60e70fcd92a59faedbb1b5c154cdc2d
Obed <adugyamfiobed.tpp3@gmail.com>
2023-06-22 20:27:49 +0100

    Variadic Functions

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,8 @@
+#ifndef VARIADIC_FUNC
+#define VARIADIC_FUNC
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *seperator, const unsigned int n, ...);
+void print_string(const char *seperator, const unsigned int n, ...);
+
+#endif
