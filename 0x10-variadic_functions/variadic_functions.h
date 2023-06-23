3b30d3961828f72f49d8725ecb6fc1974100e5a3
Obed <adugyamfiobed.tpp3@gmail.com>
2023-06-23 22:31:11 +0100

    Update

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,8 +1,10 @@
 #ifndef VARIADIC_FUNC
 #define VARIADIC_FUNC
-
+#define END_OF_LIST NULL
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *seperator, const unsigned int n, ...);
-void print_string(const char *seperator, const unsigned int n, ...);
+void print_strings(const char *seperator, const unsigned int n, ...);
+int _putchar(char c);
+void print_all(const char * const format, ...);
 
 #endif
