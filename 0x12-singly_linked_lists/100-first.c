d9789dc5936235d539b14feb9cc5872e92c733c2
Obed <adugyamfiobed.tpp3@gmail.com>
2023-07-01 02:34:00 +0100

    100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * before - Entry point.
+ * Description: Prints a string before main.
+ *
+ * Return: Void
+ *
+ */
+
+void before(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
