c77d0851508d03e7cf4d296431bde0b74158feb7
Obed <adugyamfiobed.tpp3@gmail.com>
2023-07-01 03:03:31 +0100

    Assembly Language

---
+++ 0x12-singly_linked_lists/101-hello_hoberton.asm
@@ -0,0 +1,28 @@
+/*
+This is an assembly language program that prints
+hello hoberton
+*/
+exterm printf
+
+
+section .text
+	global main
+main:
+	push rbp
+	
+	mov rdi,fmt
+	mov rsi,msg
+	mov rax, 0
+	call printf
+
+
+	pop rbp
+
+	mov rax,0
+	ret
+
+section .data
+	msg db "Hello, Horberton", 0 ; new line or 10.
+	fmt: db "%s", 0xa, 0
+
+
