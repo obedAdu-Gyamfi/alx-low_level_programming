b2c86304e12011469c49f061d649e2eac74f9aa4
Obed <adugyamfiobed.tpp3@gmail.com>
2023-07-04 21:14:09 +0100

    ):

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -32,7 +32,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		*head = (*head)->next;
 		free(current_node);
-		return (-1);
+		return (1);
 	}
 
 	while (current_node->next)
