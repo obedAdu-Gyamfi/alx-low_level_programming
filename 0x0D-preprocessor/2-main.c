b3b71032648341a0eba03fd5d8d4fc4e1165af14
Obed <adugyamfiobed.tpp3@gmail.com>
2023-08-14 12:01:12 +0100

    C preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:
+ *
+ * Return: Always 0 on success.
+ *
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,4 @@
+#ifndef FUNCTION_ONE
+#define FUNCTION_ONE
+#define ABS(x) (((x < 0) ? -(x) : (x)))
+#endif
