1b8b6fc63189caa480635f88652aaeaf0ef39162
Obed <adugyamfiobed.tpp3@gmail.com>
2023-09-04 00:37:39 +0100

    Request for correction on bit manipulation

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * flip_bits - Entry point.
+ * @n: unsigned long int.
+ * @m: unsigned long int.
+ * Return: unsigned int.
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int xclusiveOR = n ^ m;
+	unsigned int bit = 0;
+
+	if (xclusiveOR > 0)
+	{
+		bit = bit + xclusiveOR & 1;
+		xclusiveOR >>= 1;
+	}
+	return (bit);
+}
+
