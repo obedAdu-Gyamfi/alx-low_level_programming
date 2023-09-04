9c18f48c6168b7c81fe82eba368bb510764b927a
Obed <adugyamfiobed.tpp3@gmail.com>
2023-09-04 01:14:05 +0100

    Hot fix

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -11,7 +11,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned long int xclusiveOR = n ^ m;
 	unsigned int bit = 0;
 
-	if (xclusiveOR > 0)
+	while (xclusiveOR > 0)
 	{
 		bit += xclusiveOR & 1;
 		xclusiveOR >>= 1;
