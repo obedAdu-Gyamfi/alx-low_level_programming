e735f2f0ddc7bac8f282b1f0c79f01f25902967a
Obed <adugyamfiobed.tpp3@gmail.com>
2023-09-28 23:43:10 +0100

    flipping, printing and getting bit values at a given index

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * get_bit - Entry point
+ * @n: bit string.
+ * @index: position to retreive bit from
+ * Return: int
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if ((sizeof(unsigned long int) * 8) < index)
+	{
+		return (-1);
+	}
+	if ((n && (1 << index)) == 0)
+	{
+		return (0);
+	}
+	return (1);
+}
