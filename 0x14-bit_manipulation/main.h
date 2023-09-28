bf253dad57612f9983545f602dca034068af8e88
Obed <adugyamfiobed.tpp3@gmail.com>
2023-09-29 00:22:33 +0100

    Endianness

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -8,4 +8,5 @@ int _putchar(char c);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
 #endif
