1c8ef0545e64a47eab23b75039aa220830a4f6df
Obed Adu-Gyamfi <adugyamfiobed.tpp3@gmail.com>
2025-02-25 00:36:45 +0000

    printing lower case alphabets using the putchar function in C in a loop

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,20 @@
-#include<stdio.h>
-#include<ctype.h>
+#include <stdio.h>
+#include <unistd.h>
 /**
  * main - Entry point
  *
  * Description: This program prints the alphabets in lowercase, followed
+ *
  * Return: Always 0 (success)
  */
 int main(void)
 {
-int al;
-for (al = 'a'; al <= 'z'; al++)
-{
-al = tolower(al);
-putchar(al);
-}
-putchar('\n');
-return (0);
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
 }
