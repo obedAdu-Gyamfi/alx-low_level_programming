6c947c8b3bc8eb855ef5392fa893c96cd96d6250
Obed Adu-Gyamfi <adugyamfiobed.tpp3@gmail.com>
2025-02-25 01:39:55 +0000

    Eliminating Specified Characters from the lowercase alphabets

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,21 +1,26 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <unistd.h>
+
 /**
  * main - Entry point
+ *
  * Description: Prints all the alphabets except e and q
+ *
  * Return: Always 0 (success)
  *
  */
 int main(void)
 {
-int alpha;
-for (alpha = 'a'; alpha <= 'z'; alpha++)
-{
-if ((alpha != 'e') && (alpha != 'q'))
-{
-putchar(alpha);
-}
-}
-putchar('\n');
-return (0);
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		if (i != 'q' && i != 'e')
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+	return (0);
 }
 
