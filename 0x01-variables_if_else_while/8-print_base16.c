b127fb112be64bb404caa5627e1d739c2a8dc3f2
Obed Adu-Gyamfi <adugyamfiobed.tpp3@gmail.com>
2025-02-26 23:07:34 +0000

    Prints the base16 characters

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,23 +1,29 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <unistd.h>
+
 /**
  * main - Entry point
- * Description: Program prints all the numbers of base 16 in lowercase.
+ *
+ * Description: Program prints all the numbers of
+ * base 16 in lowercase.
  * Only the putchar fuction is allowed to output the results.
  * The putchar fuction was used only three times.
+ *
  * Return: Always 0 (success).
  */
 int main(void)
 {
-int dec;
-int alpha;
-for (dec = '0'; dec <= '9'; dec++)
-{
-putchar(dec);
-}
-for (alpha = 'a'; alpha <= 'f'; alpha++)
-{
-putchar(alpha);
-}
-putchar('\n');
-return (0);
+	int dec;
+	int alpha;
+
+	for (dec = '0'; dec <= '9'; dec++)
+	{
+		putchar(dec);
+	}
+	for (alpha = 'A'; alpha <= 'F'; alpha++)
+	{
+	putchar(alpha);
+	}
+	putchar('\n');
+	return (0);
 }
