c28d1abe0e1ca10310d4078b20ff2d1eca642664
Obed Adu-Gyamfi <adugyamfiobed.tpp3@gmail.com>
2025-02-27 10:20:52 +0000

    Prints out _putchar using the system call write followed by new line

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,19 @@
 #include "main.h"
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point.
  *
- * Description: This C program prints out the _putchar and a new line.
+ * Description: This C program prints out
+ * _putchar and a new line.
  *
  * Return: Always 0 on success
  */
 int main(void)
 {
+	char var[] = "_putchar\n";
 
-	printf("_putchar\n");
+	write(1, var, sizeof(var) - 1);
 	return (0);
 }
