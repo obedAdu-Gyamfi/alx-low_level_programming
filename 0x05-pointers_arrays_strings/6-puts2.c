21e27c8c96eddcc2f7a3ecab14a255edca7e3357
Obed Adu-Gyamfi <adugyamfiobed.tpp3@gmail.com>
2025-03-16 12:58:08 +0000

    prints every other characters of a string literal

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * puts2- This function prints every other
+ * character of a string, starting with
+ * the first character, followed by a new line
+ * @s: String literal
+ * Return: void
+ */
+void puts2(char *s)
+{
+	unsigned int i, j, len = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		len++;
+	}
+
+	for (j = 0; j < len - 1; j++)
+	{
+		if (s[j] % 2 == 0)
+		{
+			_putchar(s[j]);
+		}
+	}
+	_putchar('\n');
+}
