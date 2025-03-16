cd5a250baf5827b5b9cc124548fb33cca988ebe9
Obed Adu-Gyamfi <adugyamfiobed.tpp3@gmail.com>
2025-03-16 13:43:02 +0000

    Corrected puts_half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -16,13 +16,19 @@ void puts_half(char *str)
 	{
 		len++;
 	}
-	for (j = len / 2; j < len; j++)
+
+	if (len % 2 != 0)
 	{
-		_putchar(str[j]);
-		if (len % 2 != 0)
+		for (n = (len - 1) / 2; n < len - 1; n++)
 		{
-			n = (len - 1) / 2;
 			_putchar(str[n]);
+		}
+	}
+	else
+	{
+		for (j = len / 2; j < len; j++)
+		{
+			_putchar(str[j]);
 		}
 	}
 	_putchar('\n');
